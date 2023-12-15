fe0f36b48bcb9b03b1b6dda696a7f33fad1379ef
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-15 14:31:13 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api';
 
+// axios.defaults.BASE_URL
 // https://pixabay.com/api/?key=41241875-626c3239215a842e842cb8043&q=yellow+flowers&image_type=photo&pretty=true
 
 export const getAllPhotos = async (q, page) => {
@@ -14,6 +15,6 @@ export const getAllPhotos = async (q, page) => {
     orientation: 'horizontal',
     safesearch: true,
   });
-  const { data } = await axios.get(`${BASE_URL}?${params}`);
+  const { data } = await axios.get(`${BASE_URL}/?${params}`);
   return data;
 };
